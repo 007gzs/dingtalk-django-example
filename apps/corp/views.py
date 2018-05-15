eaae4b6834e5f2cb40a17ea8318f7cd24b35c0d5
007 <007gzs@gmail.com>
2018-05-15 11:32:40 +0800

    增加企业钉钉部分接口

--- apps/corp/views.py
+++ apps/corp/views.py
@@ -1,7 +1,8 @@
 # encoding: utf-8
 from __future__ import absolute_import, unicode_literals
 
-from apiview.views import ViewSite
+from apiview.views import ViewSite, fields
+from rest_framework.response import Response
 
 from core import view
 
@@ -16,6 +17,48 @@ class TestSyncCorp(view.AdminApi):
     def get_context(self, request, *args, **kwargs):
         biz.sync_corp()
         return 'ok'
+
+
+@site
+class JsapiOauth(view.APIBase):
+
+    def get_context(self, request, *args, **kwargs):
+        ret = biz.client.get_jsapi_params(request.params.href)
+        ret['errcode'] = 0
+        return Response(ret)
+
+    class Meta:
+        param_fields = (
+            ('href', fields.IntegerField(help_text='href', required=True)),
+        )
+
+
+@site
+class UserInfoByCode(view.APIBase):
+
+    def get_context(self, request, *args, **kwargs):
+        ret = biz.client.user.getuserinfo(request.params.code)
+        ret['errcode'] = 0
+        return Response(ret)
+
+    class Meta:
+        param_fields = (
+            ('code', fields.IntegerField(help_text='code', required=True)),
+        )
+
+
+@site
+class UserInfoByUserId(view.APIBase):
+
+    def get_context(self, request, *args, **kwargs):
+        ret = biz.client.user.get(request.params.userid)
+        ret['errcode'] = 0
+        return Response(ret)
+
+    class Meta:
+        param_fields = (
+            ('userid', fields.IntegerField(help_text='userid', required=True)),
+        )
 
 
 urlpatterns = site.urlpatterns
